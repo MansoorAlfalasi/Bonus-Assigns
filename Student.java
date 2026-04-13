ce7fed1a605315962514412937da8007ed548441
MansoorAlfalasi <mansoormubarak50@gmail.com>
2026-04-13 14:59:58 +0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,22 @@
-
+Public class Student { 
+   private String name; 
+   private int marks; 
+   public Student(String name, int marks) { 
+       this.name = name; 
+       this.marks = marks; 
+   } 
+ public String getName() { 
+       return name; 
+   }  
+  public int getMarks() { 
+       return marks; 
+   } 
+public void setMarks(int newMarks) { 
+marks = newMarks; 
+} 
+} 
+public class StudentAnalyzer { 
+private ArrayList<Student> studentList; 
+public StudentAnalyzer(ArrayList<Student> list) { 
+studentList = list; 
+}
